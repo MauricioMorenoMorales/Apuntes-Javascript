db9d96bf476a02e050690d87fcb89961a09205d5
mauricio <mauris_amigos@hotmail.com>
2020-08-24 13:02:39 -0500

    Clase 64

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,4 @@
-
+/*
 //! -----------------------------------DOM-----------------------------------
 
 console.log(window.document);
@@ -69,3 +69,39 @@ console.log($linkDOM.dataset.description);
 console.log($linkDOM.hasAttribute('data-id'));
 
 console.clear();
+*/
+//! -----------------------------------DOM-----------------------------------
+const $linkDOM = document.querySelector('.link-dom');
+
+console.log($linkDOM.style);
+console.log($linkDOM.getAttribute('style'));
+console.log($linkDOM.style.backgroundColor);
+console.log($linkDOM.style.color);
+console.log(window.getComputedStyle($linkDOM));
+console.log(getComputedStyle($linkDOM).getPropertyValue('color'));
+
+$linkDOM.style.setProperty('text-decoration','none');
+$linkDOM.style.setProperty('display','block');
+$linkDOM.style.width = '50%';
+$linkDOM.style.textAlign = 'center'
+$linkDOM.style.setProperty('margin-left','auto');
+$linkDOM.style.setProperty('margin-right','auto')
+$linkDOM.style.setProperty('background-color','#F7DF1E')
+$linkDOM.style.padding = '1rem';
+$linkDOM.style.borderRadius = '1rem';
+//? --------------------Variables CSS--------------------
+const  $html = document.documentElement,
+     $body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue('--dark-color'),
+     varYellowColor = getComputedStyle($html).getPropertyValue('--yellow-color');
+
+console.log(varDarkColor,varYellowColor);
+
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+
+$html.style.setProperty('--dark-color','#000'); //! No funciona solo lo agrega al elemento style
+varDarkColor = getComputedStyle($html).getPropertyValue('--dark-color');
+
+$body.style.setProperty('background-color',varDarkColor)
