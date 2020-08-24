59900173f8262b1b03320f348a5a08383232dc65
mauricio <mauris_amigos@hotmail.com>
2020-08-24 15:23:34 -0500

    Clase 67

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,4 @@
-
+/*
 //! -----------------------------------DOM-----------------------------------
 
 console.log(window.document);
@@ -47,14 +47,14 @@ console.log(document.querySelector('.link-dom').getAttribute('href'));
 
 document.documentElement.lang = '中文';
 console.log(document.documentElement.lang);
-document.documentElement.setAttribute('lang','es-MX');
+document.documentElement.setAttribute('lang', 'es-MX');
 console.log(document.documentElement.lang);
 
-const $linkDOM =document.querySelector('.link-dom');
+const $linkDOM = document.querySelector('.link-dom');
 
-$linkDOM.setAttribute('target','_blank');
-$linkDOM.setAttribute('rel','noopener');
-$linkDOM.setAttribute('href','https://youtube.com/jonmircha');
+$linkDOM.setAttribute('target', '_blank');
+$linkDOM.setAttribute('rel', 'noopener');
+$linkDOM.setAttribute('href', 'https://youtube.com/jonmircha');
 console.log($linkDOM.hasAttribute('rel'));
 $linkDOM.removeAttribute('rel');
 console.log($linkDOM.hasAttribute('rel'));
@@ -62,7 +62,7 @@ console.log($linkDOM.hasAttribute('rel'));
 //? --------------------Data-atributes--------------------
 console.log($linkDOM.getAttribute('data-description'));
 console.log($linkDOM.dataset);
-$linkDOM.setAttribute('data-description','Modelo de Objeto del documento');
+$linkDOM.setAttribute('data-description', 'Modelo de Objeto del documento');
 console.log($linkDOM.dataset.description);
 $linkDOM.dataset.description = 'Prueba';
 console.log($linkDOM.dataset.description);
@@ -70,8 +70,8 @@ console.log($linkDOM.hasAttribute('data-id'));
 
 console.clear();
 
-//! -----------------------------------DOM-----------------------------------
-const $linkDOM = document.querySelector('.link-dom');
+//! -----------------------------------DOM Estilos y variables-----------------------------------
+//TODO const $linkDOM = document.querySelector('.link-dom');
 
 console.log($linkDOM.style);
 console.log($linkDOM.getAttribute('style'));
@@ -80,28 +80,88 @@ console.log($linkDOM.style.color);
 console.log(window.getComputedStyle($linkDOM));
 console.log(getComputedStyle($linkDOM).getPropertyValue('color'));
 
-$linkDOM.style.setProperty('text-decoration','none');
-$linkDOM.style.setProperty('display','block');
+$linkDOM.style.setProperty('text-decoration', 'none');
+$linkDOM.style.setProperty('display', 'block');
 $linkDOM.style.width = '50%';
 $linkDOM.style.textAlign = 'center'
-$linkDOM.style.setProperty('margin-left','auto');
-$linkDOM.style.setProperty('margin-right','auto')
-$linkDOM.style.setProperty('background-color','#F7DF1E')
+$linkDOM.style.setProperty('margin-left', 'auto');
+$linkDOM.style.setProperty('margin-right', 'auto')
+$linkDOM.style.setProperty('background-color', '#F7DF1E')
 $linkDOM.style.padding = '1rem';
 $linkDOM.style.borderRadius = '1rem';
 //? --------------------Variables CSS--------------------
-const  $html = document.documentElement,
+const $html = document.documentElement,
      $body = document.body;
 
 let varDarkColor = getComputedStyle($html).getPropertyValue('--dark-color'),
      varYellowColor = getComputedStyle($html).getPropertyValue('--yellow-color');
 
-console.log(varDarkColor,varYellowColor);
+console.log(varDarkColor, varYellowColor);
 
 $body.style.backgroundColor = varDarkColor;
 $body.style.color = varYellowColor;
 
-$html.style.setProperty('--dark-color','#000'); //! No funciona solo lo agrega al elemento style
+$html.style.setProperty('--dark-color', '#000'); //! No funciona solo lo agrega al elemento style
 varDarkColor = getComputedStyle($html).getPropertyValue('--dark-color');
 
-$body.style.setProperty('background-color',varDarkColor)
+$body.style.setProperty('background-color', varDarkColor);
+
+
+//! -----------------------------------DOM Clases CSS-----------------------------------
+
+const $card = document.querySelector('.card');
+
+console.log($card);
+console.log($card.className);
+console.log($card.classList);
+console.log($card.classList.contains('rotate-45'));
+$card.classList.add('rotate-45');
+console.log($card.classList.contains('rotate-45'));
+console.log($card.className);
+console.log($card.classList);
+$card.classList.remove('rotate-45')
+console.log($card.classList.contains('rotate-45'))
+$card.classList.toggle('rotate-45');
+console.log($card.classList.contains('rotate-45'));
+$card.classList.toggle('rotate-45');
+console.log($card.classList.contains('rotate-45'));
+$card.classList.add('rotate-45');
+$card.classList.replace('rotate-45','rotate-135');
+$card.classList.add('opacity-80','sepia');
+$card.classList.remove('rotate-135');
+
+console.clear();
+
+//! -----------------------------------DOM Texto HTML-----------------------------------
+const $whatIsDOM = document.querySelector('.que-es')
+
+let text =`
+     <p>
+     El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model </i></b>) es un
+     API para documentos HTML y XML.
+     </p>
+     <p>
+     Éste provée una representación estructural del documento, permitiendo modificar su contenido y presentación visual mediante código JS.
+     </p>
+     <p>
+          <mark> El DOM no es parte de la especificación de JavaScript, es una API para los navegadores.</mark>
+     </p>
+`
+//$whatIsDOM.innerText = text;
+$whatIsDOM.textContent = text;
+$whatIsDOM.innerHTML = text;
+*/
+//! -----------------------------------DOM Traversing-----------------------------------
+const $cards = document.querySelector('.cards');
+
+console.log($cards);
+console.log($cards.children);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousElementSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest('div'));
+console.log($cards.closest('body'));
+console.log($cards.children[3].closest('section'));
