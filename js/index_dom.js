4c13897084f7089f0d0bf7650e7559710afe0dfe
mauricio <mauris_amigos@hotmail.com>
2020-08-27 16:18:21 -0500

    Clase 81 completada

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,7 @@
-const { default: hamburguerMenu } = require("./menu_hamburguesa.js");
+import hamburguerMenu from "./dom/menu_hamburguesa.js"
+// const { default: hamburguerMenu } = require("./menu_hamburguesa.js");
 const d = document;
 
-d.addEventListener('DOMConentLoaded',(e)=>{
-     hamburguerMenu(".panel-btn","panel");
+d.addEventListener("DOMContentLoaded",(e)=>{
+     hamburguerMenu(".panel-btn",".panel",".menu a");
 });
