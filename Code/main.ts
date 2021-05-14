1f93edd10ce97f2d1487bb18850bf94991f97c66
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-05-13 22:19:17 -0500

    Testing something

--- Code/main.ts
+++
@@ -1,7 +0,0 @@
-const defineSuit = function (card: string): string {
-	if (/♣/i.test(card)) return 'clubs'
-	if (/♠/i.test(card)) return 'spades'
-	if (/♦/i.test(card)) return 'diamonds'
-	if (/♥/i.test(card)) return 'hearts'
-	return 'Not defined'
-}
