1c8b330b0ec8becb7e43661c1db88c35f80b3912
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-24 13:42:59 -0500

    Last test remaining

--- Code/main.js
+++ Code/main.js
@@ -3,7 +3,11 @@ const solve = (firstParameter, secondParameter) => {
     const a = [...firstParameter].sort();
     const b = [...secondParameter].sort();
     let response = [];
-    for (let i = 0; i < a.length; i++) {
-        const element = array[i];
+    for (let i = 0; i < a.length || i < b.length; i++) {
+        if (a[i] !== b[i]) {
+            response.push(a[i]);
+            response.push(b[i]);
+        }
     }
+    return response.join('');
 };
--- Code/main.ts
+++ Code/main.ts
@@ -2,7 +2,14 @@ const solve = (firstParameter: string, secondParameter: string): string => {
 	const a = [...firstParameter].sort();
 	const b = [...secondParameter].sort();
 	let response: Array<string> = [];
-	for (let i = 0; i < a.length; i++) {
-		const element = array[i];
+	for (let i = 0; i < a.length || i < b.length; i++) {
+		if (a[i] !== b[i]) {
+			response.push(a[i]);
+			response.push(b[i]);
+		}
 	}
+	return response.join('');
 };
+
+// Completar
+//https://www.codewars.com/kata/5a262cfb8f27f217f700000b/train/javascript
