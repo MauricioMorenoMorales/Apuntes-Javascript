6900c3c9315f4008415919ac30896fa964c352b0
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-09-27 22:32:51 -0500

    Linea 100 typescript 2

--- Code/main.js
+++ Code/main.js
@@ -1,1 +1,14 @@
 "use strict";
+const duplicateEncode2 = (word, register = word
+    .toLowerCase()
+    .split('')
+    .reduce((responseObject, letter) => {
+    letter in responseObject
+        ? responseObject[letter]++
+        : (responseObject[letter] = 1);
+    return responseObject;
+}, {})) => word
+    .toLowerCase()
+    .split('')
+    .map(letter => (register[letter] >= 2 ? ')' : '('))
+    .join('');
--- Code/main.ts
+++ Code/main.ts
@@ -1,1 +1,18 @@
+const duplicateEncode2 = (
+	word: string,
+	register = word
+		.toLowerCase()
+		.split('')
+		.reduce((responseObject: any, letter) => {
+			letter in responseObject
+				? responseObject[letter]++
+				: (responseObject[letter] = 1);
+			return responseObject;
+		}, {}),
+): string =>
+	word
+		.toLowerCase()
+		.split('')
+		.map(letter => (register[letter] >= 2 ? ')' : '('))
+		.join('');
 
